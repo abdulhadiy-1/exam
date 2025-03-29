8fee34ca7309b4687135da930bc7e6790de31110
abdulhadiy-1 <hadiyhadiy2008@gmail.com>
2025-03-29 11:56:53 +0500

    last

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,5 +1,5 @@
 const winston = require("winston");
-// require("winston-mongodb");
+require("winston-mongodb");
 
 const { json, combine, timestamp } = winston.format;
 
@@ -12,11 +12,11 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: "logger.log", level: "error" }),
     new winston.transports.Console(),
-    // new winston.transports.MongoDB({
-    //   db: "mongodb://localhost:27017/logger",
-    //   options: { useUnifiedTopology: true },
-    //   collection: "logger",
-    // }),
+    new winston.transports.MongoDB({
+      db: "mongodb+srv://hadiyhadiy2008:fIu8XNAtpKPJ6d0E@cluster0.6xf2s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      options: { useUnifiedTopology: true },
+      collection: "logger",
+    }),
   ],
 });
 
