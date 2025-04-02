8cd3df5d3dd547f41fecb413ee19e69c0de4a022
abdulhadiy-1 <hadiyhadiy2008@gmail.com>
2025-04-02 15:59:00 +0500

    changed

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ async function connectDb() {
   try {
     await db.authenticate();
     console.log("Connection has been established successfully.");
-    await db.sync({alter: true})
+    await db.sync({force: true})
     console.log("All models were synchronized successfully.");
   } catch (error) {
     console.log("Unable to connect to the database:", error);
